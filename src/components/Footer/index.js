e36a17ae71cef5992c89760e42e53abe97eb3957
Auzan <auzan.ip@gmail.com>
2022-03-08 13:07:44 +0700

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,33 +1,49 @@
-import React from 'react';
+import React from "react";
 
-import './style.css'
+import "./style.css";
 
-
-function Index( props ) {
-    return (
-        <footer className="col-12 footer">
-            <div className="justify-content-center">
-                <div className="col-12 text-light">
-                    <ul className="list-inline d-flex footer-list">
+function Index(props) {
+  return (
+    <footer className="col-12 footer">
+      <div className="justify-content-center">
+        <div className="col-12 text-light">
+          {/* <ul className="list-inline d-flex footer-list">
                         <li>
                             <a target="_blank" rel="noreferrer" href="https://s.id/geschool" className="footer-link bold">Road Map to Geschool</a>
                         </li>
-                    </ul>
-                    <ul className="list-inline d-flex footer-list link-list">
-                        <li>
-                            <a target="_blank" rel="noreferrer" href="https://padangpariamankab.go.id/" className="footer-link">Pemerintah Kabupaten Padang Pariaman</a>
-                        </li>
-                        <li>
-                            <a target="_blank" rel="noreferrer" href="http://disdikbud.padangpariamankab.go.id/ " className="footer-link">Disdikbud Pariaman</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-12 copyright mt-2">
-                    &copy; {props.copyright}
-                </div>
-            </div>
-        </footer>
-    );
+                    </ul> */}
+          <ul className="list-inline d-flex footer-list link-list">
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://padangpariamankab.go.id/"
+                className="footer-link"
+              >
+                Pemerintah Kabupaten Padang Pariaman
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="http://disdikbud.padangpariamankab.go.id/ "
+                className="footer-link"
+              >
+                Disdikbud Pariaman
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 copyright mt-2">
+          &copy; {props.copyright}{" "}
+          <a target="_blank" rel="noreferrer" href="https://s.id/geschool">
+            Geschool
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Index;
--- src/layouts/PageLayout/index.js
+++ src/layouts/PageLayout/index.js
@@ -1,44 +1,40 @@
-
 //NOT EDITABLE!!!
-import Topography from "../../components/Topography"
-import Hero from "../../components/Hero"
-import Footer from "../../components/Footer"
+import Topography from "../../components/Topography";
+import Hero from "../../components/Hero";
+import Footer from "../../components/Footer";
 
 // EDITABLE
-import background from "../../assets/img/background.jpg" //Background
-import backgroundMobile from "../../assets/img/backgroundM.jpg" //BackgroundMobile
-import logo from '../../assets/img/logo.png'; //Logo
+import background from "../../assets/img/background.jpg"; //Background
+import backgroundMobile from "../../assets/img/backgroundM.jpg"; //BackgroundMobile
+import logo from "../../assets/img/logo.png"; //Logo
 
+function pageLayout(props) {
+  //EDITABLE
+  const deskripsi = "Selamat datang kembali di";
+  const title1 = "Padang Pariaman";
+  const title2 = "Blended Learning";
+  const slogan =
+    "Jika kau menginginkan sesuatu dalam hidupmu yang tak pernah kau punya. Kau harus melakukan sesuatu yang belum pernah kau lakukan. ";
+  const copyright = "2022 Padang Pariaman Blended Learning - Didukung oleh";
 
-function pageLayout( props ) {
-    //EDITABLE
-    const deskripsi = "Selamat datang kembali di"
-    const title1 = "Padang Pariaman"
-    const title2 = "Blended Learning"
-    const slogan = "Jika kau menginginkan sesuatu dalam hidupmu yang tak pernah kau punya. Kau harus melakukan sesuatu yang belum pernah kau lakukan. "
-    const copyright = "2022 Padang Pariaman Blended Learning - Didukung oleh Geschool"
-
-    return (
-        <div>
-            <Topography />
-            <Hero 
-                logo={logo}
-                background={background}
-                backgroundMobile={backgroundMobile}
-                deskripsi={deskripsi}
-                title1={title1}
-                title2={title2}
-                slogan={slogan}
-                appName={props.appName}
-            >
-                { props.children }
-            </Hero>
-            <Footer
-                copyright={copyright}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Topography />
+      <Hero
+        logo={logo}
+        background={background}
+        backgroundMobile={backgroundMobile}
+        deskripsi={deskripsi}
+        title1={title1}
+        title2={title2}
+        slogan={slogan}
+        appName={props.appName}
+      >
+        {props.children}
+      </Hero>
+      <Footer copyright={copyright} />
+    </div>
+  );
 }
-
 
 export default pageLayout;
